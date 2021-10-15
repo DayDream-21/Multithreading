9185a35ed92d7d83052f1851aad1a2d7a3b2ba03
DayDream-21 <vova13158@gmail.com>
2021-10-16 00:44:26 +0300

    Created 2 non-synchronized threads

--- src/main/java/com/slavamashkov/multithreading/alishev_course/MyThreadTest.java
+++ src/main/java/com/slavamashkov/multithreading/alishev_course/MyThreadTest.java
@@ -1,26 +1,30 @@
 package com.slavamashkov.multithreading.alishev_course;
 
+import java.util.Random;
+
 public class MyThreadTest {
     public static void main(String[] args) {
-        Runnable task1 = () -> {
-            for (int i = 0; i < 10; i++) {
-                System.out.println("Hello from task1");
-            }
-        };
+        MyThread myThread1 = new MyThread();
+        MyThread myThread2 = new MyThread();
 
-        Thread thread = new Thread(task1);
-        thread.start();
-
-        MyThread myThread = new MyThread();
-        myThread.start();
+        myThread1.start();
+        myThread2.start();
     }
 }
 
 class MyThread extends Thread {
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello from MyThread");
+        for (int i = 0; i < 100; i++) {
+            Random random = new Random();
+            try {
+                int time = random.nextInt(100);
+                Thread.sleep(time);
+                System.out.println("Hello from MyThread #" + i +
+                        " I'm going to sleep for " + time + "millis");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
